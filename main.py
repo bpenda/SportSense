930bd8d67ce9baa666b193508a2a3e6ff986c6dd
Bill <bpenda94@gmail.com>
2016-04-28 21:00:17 -0500

    different library for mpu6050

---
+++ main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+"""
+Released under the MIT License
+Copyright 2015 MrTijn/Tijndagamer
+"""
+
+# Import the MPU6050 class from the MPU6050.py file
+from MPU6050 import MPU6050
+from time import sleep
+
+set_accel_range(self, ACCEL_RANGE_16G)
+# Create a new instance of the MPU6050 class
+sensor = MPU6050(0x68)
+
+bus.write_byte_data(address, power_mgmt_1, 0)
+
+f = open('data.csv', 'w')
+cur_time = time.time()
+
+while True:
+    accel_data = sensor.get_accel_data()
+    gyro_data = sensor.get_gyro_data()
+    cur_time = time.time()
+
+
+    print(str(accel_data['x']))
+    print(",")
+    print(str(accel_data['y']))
+    print(",")
+    print(str(accel_data['z']))
+    print("\n")
+
+    f.write(str(int(round(cur_time*1000))))
+    f.write(",")
+    f.write(str(gyro_data['x'] / 131))
+    f.write(",")
+    f.write(str(gyro_data['y'] / 131))
+    f.write(",")
+    f.write(str(gyro_data['z'] / 131))
+    f.write(",")
+    f.write(str(accel_data['x']))
+    f.write(",")
+    f.write(str(accel_data['y']))
+    f.write(",")
+    f.write(str(accel_data['z']))
+    f.write('\n')
